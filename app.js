2ede25a8b9ddd586574f70aa97fabae41fc7b759
catherineisonline <ekaterine.jpg@gmail.com>
2022-01-10 18:06:55 +0400

    fix close btn

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 const modalBtn = document.querySelector(".modal-btn");
 const modalOverlay = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
+const banner = document.querySelector('.banner');
 
 //Open Action
 modalBtn.addEventListener("click", function () {
   modalOverlay.classList.add("open-modal");
+  banner.classList.add('hidden');
 });
 
 //Close action
 const closeAction = function () {
   modalOverlay.classList.remove("open-modal");
+  banner.classList.remove('hidden');
 };
 
 //Close with X
