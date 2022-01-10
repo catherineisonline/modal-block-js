3f96d7aa6cd8dca5d267d1360fe0e4add272fc9d
catherineisonline <ekaterine.jpg@gmail.com>
2022-01-10 17:43:30 +0400

    added close button actions

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+"use strict";
+
 const modalBtn = document.querySelector(".modal-btn");
-const modal = document.querySelector(".modal-overlay");
+const modalOverlay = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
-
+//Open Action
 modalBtn.addEventListener("click", function () {
-    modal.classList.add("open-modal");
+  modalOverlay.classList.add("open-modal");
 });
 
+//Close action
+const closeAction = function () {
+  modalOverlay.classList.remove("open-modal");
+};
 
-closeBtn.addEventListener("click", function () {
-    modal.classList.remove("open-modal");
+//Close with X
+closeBtn.addEventListener("click", closeAction);
+
+//Close outside X
+modalOverlay.addEventListener("click", closeAction);
+
+//Close with ESC key
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    return closeAction();
+  }
 });
